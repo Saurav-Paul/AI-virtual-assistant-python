3ee5e0b45e49d97ab32880824250c2655a8a7c09
Saurav Paul <sauravpaul.sunny@gmail.com>
2020-06-05 19:09:01 +0600

    makes comatible for windows

--- run.py
+++ run.py
@@ -4,12 +4,12 @@ from settings.settings import read_bot
 
 def get_args(lt,lim):
     get = ''
-    for i in range(2,lim):
+    for i in range(1,lim):
         get += str(lt[i]) + ' '
     return get.strip()
 
 def all_args(lt):
-    print(lt)
+    # print(lt)
     ok = False
     arg = ''
     for w in lt:
@@ -35,18 +35,24 @@ if __name__ == "__main__":
     try :
         total = len(sys.argv)
         lt = list(sys.argv)
-        orginal_path = lt[0]
-        orginal_path = orginal_path[0:len(orginal_path)-6]
-        # going to change a few bit -_-
-        p = str(sys.argv)
-        # x = p.split(sep='-arg')
-        # print(x)
-        # read_bot(orginal_path)
-        file_path = make_path(lt,total)
-        os.chdir(file_path)
-        # print(file_path)
-        arg = all_args(lt)
-        main(arg,orginal_path)
+        if '-arg' in lt:
+            orginal_path = lt[0]
+            orginal_path = orginal_path[0:len(orginal_path)-6]
+            # going to change a few bit -_-
+            p = str(sys.argv)
+            # x = p.split(sep='-arg')
+            # print(x)
+            # read_bot(orginal_path)
+            file_path = make_path(lt,total)
+            # print(lt)
+            os.chdir(file_path)
+            # print(file_path)
+            arg = all_args(lt)
+            main(arg,orginal_path)
+        else :
+            arg = get_args(lt,len(lt))
+            # print(arg)
+            main(arg.strip() , orginal_path=os.getcwd())
         # print(f'all the args {arg}.')
         # print(lt)
         # os.chdir(sys.argv[1])
