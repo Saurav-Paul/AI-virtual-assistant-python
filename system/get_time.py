32405c0a516b0c926dab66b4e12ed2a0e16fd1af
Saurav Paul <sauravpaul.sunny@gmail.com>
2020-06-08 20:47:35 +0600

    updated

--- system/get_time.py
+++ system/get_time.py
@@ -3,10 +3,10 @@ from datetime import datetime
 import time
 
 def get_time():
-    return datetime.now().strftime('%H:%M:%S %d-%m-%Y')
-
+    return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
 def digital_time():
-    return time.strftime("%b %d %Y %-I:%M %p")
+    return time.strftime("%B %d, %Y %-I:%M %p")
 
 if __name__ == "__main__":
+    print(get_time())
     print(digital_time())
