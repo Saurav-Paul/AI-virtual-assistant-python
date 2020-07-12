5f39fbbf283af657785af57973d1d1e9862fd249
sauravpaul.sunny@gmail.com <sauravpaul.sunny@gmail.com>
2020-07-12 22:01:01 +0600

    voice command features installatoin fixed

--- system/features_installation.py
+++ system/features_installation.py
@@ -41,12 +41,25 @@ def install_speaking_system():
     pt = '-'*17 + "Installing speaking system" + '-'*17
     cprint(pt,'magenta')
     print()
-    s =  """ Packages are need to install,\n\n\t1)pyttsx3\n\n If you face any problem, install them manually.\n"""
+    s =  """ Packages are need to install,\n\n\t1)pyttsx3\n\n\t2)espeak/gespeaker\n\n If you face any problem, install them manually.\n"""
     cprint(s,'yellow')
 
     if 'linux' in x:
         cprint("System : Linux",'green')
         os.system('pip3 install pyttsx3')
+        cprint('\nWhich linux distribution are you using ? ','cyan')
+        cprint('\n 1)debian-based\n 2)Arch-based\n 3)Other')
+        cprint('\nEnter the index number : ','cyan',end='')
+        index = int(input())
+        if index == 2 :
+            cprint(' Installing Gespeaker ...','green')
+            os.system('yay -S gespeaker')
+        elif index == 1:
+            cprint(' Installing espeak ...','green')
+            os.system('sudo apt-get install espeak')
+        else :
+            cprint(" If your voice reply don't work you need install espeak/gespeaker manually",'red')
+
     elif 'darwin' in x:
         cprint("System : Mac Os",'green')
         os.system('pip3 install pyttsx3')
