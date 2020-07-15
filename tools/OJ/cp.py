d4d70d4a08bba191c556ecabe842e8cdd244f37d
sauravpaul.sunny@gmail.com <sauravpaul.sunny@gmail.com>
2020-07-15 16:28:56 +0600

    Cp different table added

--- tools/OJ/cp.py
+++ tools/OJ/cp.py
@@ -33,6 +33,152 @@ class bcolors:
     UNDERLINE = '\033[4m'
 
 
+class table:
+    box_weight = 50
+
+    table_color = 'white'
+    keyword = 'yellow'
+    accepted = 'green'
+    wrong = 'red'
+    information = 'white'
+
+    dif_sign = clr('|',table_color,attrs=['bold'])
+
+    def multiple(self,n,value= ' ') :
+        s = value*n
+        return s
+
+    def separator(self,value='-') :
+        cprint(self.multiple(self.box_weight*2+3+7,clr(value,self.table_color,attrs=['bold']) ),self.table_color)
+
+    def header(self,col1,col2):
+
+        self.separator()
+
+
+        print(self.dif_sign +clr(' Line ',self.keyword)+ self.dif_sign , end='')
+
+        before = (self.box_weight - len(col1))/2
+        before = int(before)
+        after = self.box_weight - before - len(col1)
+
+        print(self.multiple(before,' ')+clr(col1,self.keyword)+self.multiple(after,' '),end='')
+        print(self.dif_sign,end='')
+
+        before = (self.box_weight - len(col2))/2
+        before = int(before)
+        after = self.box_weight - before - len(col2)
+
+        print(self.multiple(before)+clr(col2,self.keyword)+self.multiple(after),end='')
+        print(self.dif_sign,end='')
+
+        print()
+
+        self.separator()
+
+    def line_print(self,no,x,y) :
+
+
+        # p =''
+
+        # for o , e in zip_longest(x,y,fillvalue=''):
+        #     if(o == e):
+        #         p += clr(o,self.accepted)
+        #     else :
+        #         p += clr(o,self.wrong)
+
+
+        pt =[] 
+
+        for o , e in zip_longest(x,y,fillvalue=''):
+            if(o == e):
+                pt.append(clr(o,self.accepted) )
+            else :
+                pt.append(clr(o,self.wrong))
+
+
+        sx = len(x)
+        sy = len(y)
+        curr = 0 
+
+        xNull = False
+        yNull = False
+
+        if x == '(#$null$#)' :
+            xNull = True
+
+        if y == '(#$null$#)' :
+            yNull = True
+        
+
+        smax = max(sx,sy)
+
+        while curr <= smax :
+            print(self.dif_sign + ' ' + clr(no,'cyan') + ' '*(5 - len(no)) + self.dif_sign,end = '')
+            tx = ''
+            if xNull == True :
+                tx = clr('(null)',self.information) + ' ' * (self.box_weight - 6)
+            else :
+                for i in range(curr,curr+self.box_weight):
+                    if i < sx :
+                        tx += pt[i]
+                    else:
+                        tx += ' ' * (self.box_weight - (i - curr ))
+                        break
+
+            print(tx + self.dif_sign,end='')
+
+
+            tx = ''
+            if yNull == True :
+                tx = clr('(null)',self.information) + ' ' * (self.box_weight - 6)
+            else :
+                for i in range(curr,curr+self.box_weight):
+                    if i < sy :
+                        tx += clr(y[i],self.accepted)
+                    else:
+                        tx += ' ' * (self.box_weight - (i - curr ))
+                        break
+        
+
+            print(tx + self.dif_sign)
+
+            curr += self.box_weight
+            no = ''
+                
+        # after = self.box_weight - len(x)
+        # print(p+after*' ' + self.dif_sign, end = '')
+
+        # after = self.box_weight - len(y)
+        # print(clr(y,self.accepted)+after*' ' + self.dif_sign)
+
+        # self.separator('-')
+
+    def print(self,output,expected,col1='Output',col2='Expected'):
+
+        self.header(col1,col2)
+
+        x = output.split(sep='\n')
+        y = expected.split(sep='\n')
+
+        sx = len(x)
+        sy = len(y)
+
+        total_line = max(sx,sy)
+
+        for no in range(total_line) : 
+            try :
+                vx = x[no]
+            except :
+                vx = '(#$null$#)'
+            try :
+                vy = y[no]
+            except :
+                vy = '(#$null$#)'
+            self.line_print(str(no+1),vx,vy)
+
+        self.separator()
+
 
 class Cp_my_tester:
 
@@ -66,8 +212,11 @@ class Cp_my_tester:
         # print(value)
         self.diff_print('Input',i,'cyan')
         # self.diff_print('Output',x)
-        self.colorfull_diff_print(x,y)
-        self.diff_print('Expected',y,'green')
+        # self.colorfull_diff_print(x,y)
+        # self.diff_print('Expected',y,'green')
+
+        obj = table()
+        obj.print(output,expected)
         # print('Output :')
         # print(output)
         # print("Expected :")
